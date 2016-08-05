32317f167efea01c431da3742bddc44e05e73aac
Chris <clai88@gmail.com>
2016-08-04 21:45:52 -0400

    able to post tweets

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,2 +1,26 @@
 class TweetsController < ApplicationController
+  def index
+    @tweets = Tweet.where(user_id: session[:user_id])
+  end
+
+  def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    @tweet = Tweet.new(tweet_params)
+    @tweet.user_id = session[:user_id]
+
+    if @tweet.save
+      redirect_to tweets_path
+    else
+      render :new
+    end
+  end
+
+  private def tweet_params
+    params.require("tweet").permit(:body, :user_id)
+  end
+
+
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,5 @@
 class WelcomeController < ApplicationController
   def index
+    @tweets = Tweet.all
   end
 end
