e63dedde6f13c9c30ce264650e2eeeb2c6116dc2
Chris <clai88@gmail.com>
2016-08-04 21:16:20 -0400

    added tweet controller and model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160804205046) do
+ActiveRecord::Schema.define(version: 20160805011344) do
+
+  create_table "tweets", force: :cascade do |t|
+    t.string   "body"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
