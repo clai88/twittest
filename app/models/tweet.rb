28c40c1fd8dbc9416d5902933d299c3280de8eb8
Chris <clai88@gmail.com>
2016-08-06 11:47:08 -0400

    cleaned up bug with displaying time since tweet

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,29 +1,33 @@
 class Tweet < ActiveRecord::Base
   belongs_to :user
 
+  validates :body, length: { maximum: 170, too_long: "Unfortunately, 170 characters is the maximum allowed.  Please shorten your teet"  }
+
   def time
     a = self.updated_at
     b = Time.now
-    diff = TimeDifference.between(a,b).in_hours.to_i
-    diff_min = TimeDifference.between(a,b).in_minutes.to_i
+    # diff = TimeDifference.between(a,b).in_hours.to_i
+    diff = TimeDifference.between(a,b).in_minutes.to_i
+    base = TimeDifference.between(a,b)
 
     case diff
     when 0
-      if diff_min == 0
-        time = TimeDifference.between(a,b).in_seconds.to_i
-        "#{time}s"
-      else
-        diff_min
-        "#{diff_min}m"
-      end
-    when 0...24
-      "#{diff}h"
-    when 24...168
+      time = TimeDifference.between(a,b).in_seconds.to_i
+      "#{time}s"
+    when 0...60
+      diff
+      "#{diff}m"
+    when 60...1440
+      time = TimeDifference.between(a,b).in_hours.to_i
+      "#{time}h"
+    when 1440...10_080
+      time = TimeDifference.between(a,b).in_days.to_i
+      "#{time}d"
+    when 10_080...43_200
       time = TimeDifference.between(a,b).in_weeks.to_i
       "#{time}w"
     else
       a.to_formatted_s(:short)
     end
   end
-
 end
