50a9279ebb045169c5dcdba175c88fb927a0380e
Chris <clai88@gmail.com>
2016-08-06 15:09:43 -0400

    added min length for password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,11 @@
 class User < ActiveRecord::Base
   validates :email, format: {with: /@/}, uniqueness: true
   validates :password_digest, presence: true
-  validates :password, confirmation: true
+  validates :password, length: { minimum: 6, too_short: "Please make your password at least 6 characters long"  },confirmation: true
 
   def self.sign_up(user_hash)
     User.create(user_hash)
   end
 
-  has_secure_password
-
-  # def password=(other)
-  #   digest = BCrypt::Password.create(other)
-  #   self.password_digest = digest
-  # end
-  #
-  # def password
-  #   BCrypt::Password.new(self.password_digest)
-  # end
-
+  has_secure_password #new way
 end
