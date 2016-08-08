54430aceed2068d5bd9d0922ce716bb9944144c0
Chris <clai88@gmail.com>
2016-08-08 08:57:36 -0400

    refactored follower_user#create

--- app/controllers/follow_users_controller.rb
+++ app/controllers/follow_users_controller.rb
@@ -21,16 +21,18 @@ class FollowUsersController < ApplicationController
     @twets = @tweets.sort_by{ |tweet| tweet.updated_at }
   end
   def create
-    if session[:user_id] == params[:user_to_follow_id].to_i
+    follower = session[:user_id]
+    followee = params[:user_to_follow_id].to_i
+    if follower == followee
       flash[:notice] = "You cannot follow yourself"
     else
-      FollowUser.create(follower_id: session[:user_id],followee_id: params[:user_to_follow_id])
-      follower = User.find_by(id: session[:user_id])
-      followee = User.find_by(id: params[:user_to_follow_id])
+      FollowUser.create(follower_id: follower,followee_id: followee)
+      follower = User.find_by(id: follower)
+      followee = User.find_by(id: followee)
       UserMailer.new_follower_email(follower,followee).deliver_now
     end
 
-    redirect_to profile_path(params[:user_to_follow_id])
+    redirect_to profile_path(followee)
   end
 
   def destroy
